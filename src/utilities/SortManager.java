95747ca2b1bc10d3b20ba555bebfc2ffa02baa55
CrusaderJohnny <17ark.leonardis@gmail.com>
2025-02-26 13:55:27 -0700

    Documented sort manager methods

--- src/utilities/SortManager.java
+++ src/utilities/SortManager.java
@@ -2,11 +2,18 @@ package utilities;
 
 import java.util.Comparator;
 
-import shapes.BaseAreaCompare;
-import shapes.Shapes;
-import shapes.VolumeCompare;
+/**
+ * Sorting Class that holds all sorting methods for the application
+ */
+
 
 public class SortManager {
+	
+	/**
+	 * Takes an array and sorts using the bubble sort method
+	 * @param <T> Takes Object class
+	 * @param arr array of objects
+	 */
 
 	public static <T extends Comparable<T>>void bubbleSort(T[] arr) {
 		boolean swap;
@@ -26,6 +33,13 @@ public class SortManager {
 			}
 		}
 	}
+	
+	/**
+	 * Bubble sort method that takes an array and comparator method
+	 * @param <T> Object class
+	 * @param arr Array of objects
+	 * @param c Passed comparator method
+	 */
 
 	public static <T> void bubbleSort(T[] arr, Comparator<T> c) {
 		boolean swap;
@@ -45,42 +59,98 @@ public class SortManager {
 			}
 		}
 	}
+	
+	/**
+	 * Insertion sort method taking an array of objects
+	 * @param <T> Takes Object class
+	 * @param arr array of objects
+	 */
 	public static <T extends Comparable<T>> void insertionSort(T[] arr) {
 		//to do
 	}
 	
+	/**
+	 * Insertion sort method taking an array of objects and a comparator
+	 * @param <T> Object class
+	 * @param arr Array of objects
+	 * @param c Passed comparator method
+	 */
 	public static <T> void insertionSort(T[] arr, Comparator<T> c) {
 		// to do
 	}
 	
+	/**
+	 * Selection sort method taking an array of objects
+	 * @param <T> Takes Object class
+	 * @param arr array of objects
+	 */
 	public static <T extends Comparable<T>> void selectionSort(T[] arr) {
 		// to do
 	}
 	
+	/**
+	 * Selection sort method taking an array of objects and a comparator
+	 * @param <T> Object class
+	 * @param arr Array of objects
+	 * @param c Passed comparator method
+	 */
 	public static <T> void selectionSort(T[] arr, Comparator<T> c) {
 		// to do
 	}
 	
+	/**
+	 * Merge sort method taking an array of objects
+	 * @param <T> Takes Object class
+	 * @param arr array of objects
+	 */
 	public static <T extends Comparable<T>> void mergeSort(T[] arr) {
 		// to do
 	}
 	
+	/**
+	 * Merge sort method taking and array of objects and a comparator
+	 * @param <T> Object class
+	 * @param arr Array of objects
+	 * @param c Passed comparator method
+	 */
 	public static <T> void mergeSort(T[] arr, Comparator<T> c) {
 		// to do
 	}
 	
+	/**
+	 * Quick Sort Method taking an array of objects
+	 * @param <T> Takes Object class
+	 * @param arr array of objects
+	 */
 	public static <T extends Comparable<T>> void quickSort(T[] arr) {
 		// to do
 	}
 	
+	/**
+	 * Quick sort method taking an array of objects and a comparator
+	 * @param <T> Object class
+	 * @param arr Array of objects
+	 * @param c Passed comparator method
+	 */
 	public static <T> void quickSort(T[] arr, Comparator<T> c) {
 		// to do
 	}
 	
+	/**
+	 * Heap sort method taking an array of objects
+	 * @param <T> Takes Object class
+	 * @param arr array of objects
+	 */
 	public static <T extends Comparable<T>> void heapSort(T[] arr) {
 		// to do
 	}
 	
+	/**
+	 * Heap sort method taking an array of objects and a comparator
+	 * @param <T> Object class
+	 * @param arr Array of objects
+	 * @param c Passed comparator method
+	 */
 	public static <T> void heapSort(T[] arr, Comparator<T> c) {
 		// to do
 	}
