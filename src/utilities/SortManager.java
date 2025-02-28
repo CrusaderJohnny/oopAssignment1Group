36c97bc36fa690ee4695865cd73854c226bbb973
Vanshita Munday <vanshita.munday@edu.sait.ca>
2025-02-28 14:56:01 -0700

    Added Heap, Select, Insert Sort

--- src/utilities/SortManager.java
+++ src/utilities/SortManager.java
@@ -28,7 +28,7 @@ public class SortManager {
 				}
 			}
 			//no swaps in inner loop, break out 
-			if(swap = false) {
+			if(!swap) {
 				break;
 			}
 		}
@@ -54,7 +54,7 @@ public class SortManager {
 				}
 			}
 			//no swaps in inner loop, break out 
-			if(swap = false) {
+			if(!swap) {
 				break;
 			}
 		}
@@ -66,8 +66,17 @@ public class SortManager {
 	 * @param arr array of objects
 	 */
 	public static <T extends Comparable<T>> void insertionSort(T[] arr) {
-		//to do
+		for (int i = 1; i < arr.length; i++) {
+			T key = arr[i];
+			int j = i - 1;
+			while (j >= 0 && arr[j].compareTo(key) > 0) {
+				arr[j + 1] = arr[j];
+				j--;
+			}
+			arr[j + 1] = key;
+		}
 	}
+
 	
 	/**
 	 * Insertion sort method taking an array of objects and a comparator
@@ -76,7 +85,15 @@ public class SortManager {
 	 * @param c Passed comparator method
 	 */
 	public static <T> void insertionSort(T[] arr, Comparator<T> c) {
-		// to do
+		for (int i = 1; i < arr.length; i++) {
+			T key = arr[i];
+			int j = i - 1;
+			while (j >= 0 && c.compare(arr[j], key) > 0) {
+				arr[j + 1] = arr[j];
+				j--;
+			}
+			arr[j + 1] = key;
+		}
 	}
 	
 	/**
@@ -85,7 +102,17 @@ public class SortManager {
 	 * @param arr array of objects
 	 */
 	public static <T extends Comparable<T>> void selectionSort(T[] arr) {
-		// to do
+		for (int i = 0; i < arr.length - 1; i++) {
+			int minIndex = i;
+			for (int j = i + 1; j < arr.length; j++) {
+				if (arr[j].compareTo(arr[minIndex]) < 0) {
+					minIndex = j;
+				}
+			}
+			T temp = arr[minIndex];
+			arr[minIndex] = arr[i];
+			arr[i] = temp;
+		}
 	}
 	
 	/**
@@ -95,7 +122,17 @@ public class SortManager {
 	 * @param c Passed comparator method
 	 */
 	public static <T> void selectionSort(T[] arr, Comparator<T> c) {
-		// to do
+		for (int i = 0; i < arr.length - 1; i++) {
+			int minIndex = i;
+			for (int j = i + 1; j < arr.length; j++) {
+				if (c.compare(arr[j], arr[minIndex]) < 0) {
+					minIndex = j;
+				}
+			}
+			T temp = arr[minIndex];
+			arr[minIndex] = arr[i];
+			arr[i] = temp;
+		}
 	}
 	
 	/**
@@ -135,16 +172,8 @@ public class SortManager {
 	public static <T> void quickSort(T[] arr, Comparator<T> c) {
 		// to do
 	}
-	
-	/**
-	 * Heap sort method taking an array of objects
-	 * @param <T> Takes Object class
-	 * @param arr array of objects
-	 */
-	public static <T extends Comparable<T>> void heapSort(T[] arr) {
-		// to do
-	}
-	
+
+
 	/**
 	 * Heap sort method taking an array of objects and a comparator
 	 * @param <T> Object class
@@ -152,6 +181,67 @@ public class SortManager {
 	 * @param c Passed comparator method
 	 */
 	public static <T> void heapSort(T[] arr, Comparator<T> c) {
-		// to do
+	    int n = arr.length;
+
+	    // Build max heap
+	    for (int i = n / 2 - 1; i >= 0; i--) {
+	        heapify(arr, n, i, c);
+	    }
+
+	    // Extract elements one by one
+	    for (int i = n - 1; i > 0; i--) {
+	        swap(arr, 0, i);
+	        heapify(arr, i, 0, c);
+	    }
 	}
+
+	/**
+	 * Heapify method for Comparable version
+	 */
+	private static <T extends Comparable<T>> void heapify(T[] arr, int n, int i) {
+	    int largest = i;
+	    int l = 2 * i + 1; // Left child
+	    int r = 2 * i + 2; // Right child
+
+	    if (l < n && arr[l].compareTo(arr[largest]) > 0) {
+	        largest = l;
+	    }
+	    if (r < n && arr[r].compareTo(arr[largest]) > 0) {
+	        largest = r;
+	    }
+	    if (largest != i) {
+	        swap(arr, i, largest);
+	        heapify(arr, n, largest);
+	    }
+	}
+
+	/**
+	 * Heapify method for Comparator version
+	 */
+	private static <T> void heapify(T[] arr, int n, int i, Comparator<T> c) {
+	    int largest = i;
+	    int l = 2 * i + 1; // Left child
+	    int r = 2 * i + 2; // Right child
+
+	    if (l < n && c.compare(arr[l], arr[largest]) > 0) {
+	        largest = l;
+	    }
+	    if (r < n && c.compare(arr[r], arr[largest]) > 0) {
+	        largest = r;
+	    }
+	    if (largest != i) {
+	        swap(arr, i, largest);
+	        heapify(arr, n, largest, c);
+	    }
+	}
+
+	/**
+	 * Swap utility function
+	 */
+	private static <T> void swap(T[] arr, int i, int j) {
+	    T temp = arr[i];
+	    arr[i] = arr[j];
+	    arr[j] = temp;
+	}
+
 }
